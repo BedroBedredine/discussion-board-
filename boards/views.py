cc3ff9ca4c0059f22cc304e2cfe7b08da7e14d69
BedroBedredine <bedroghanoubedredine@gmail.com>
2020-09-23 20:16:48 +0100

    1st ripo push

---
+++ boards/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,Http404
+from .models import Board
+# Create your views here.
+
+def home(request):
+    boards = Board.objects.all()
+    return render(request,'home.html',{'boards':boards})
+
+def board_topics(request,board_id):
+    # try:
+    #     board = Board.objects.get(pk=board_id)
+    # except board.DoesNotExist:
+    #     raise Http404
+    board = get_object_or_404(Board,pk=board_id)
+    return render(request,'topics.html',{'board':board})
